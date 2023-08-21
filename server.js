b2b7b09282d7ef27de5639b44bb05671740151eb
Pranavumak13 <2020bcs077@sggs.ac.in>
2023-08-21 16:42:59 +0530

    feat - add nice user interface

--- server.js
+++ server.js
@@ -9,25 +9,30 @@ const http = require('http').Server(app); // attach the 'app' to http module
 
 const port = process.env.PORT || 3001;
 
+const localIP = '192.168.213.139';
+
+
 // attach http server to socket.io
-const io = require('socket.io')(http);      
+const io = require('socket.io')(http);
+
+
 
 
 //route
-app.get('/', (req,res)=>{
-    res.sendFile(path.join(__dirname + '/client/index.html')  )
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/client/index.html'))
 })
 
 // create a connection from the server side
 // .on(event, eventListner)
 
 
-io.on('connection',socket=>{
+io.on('connection', socket => {
     console.log('a user connected');
     console.log(socket.id);
-    
-    
-    socket.on('disconnect',()=>{
+
+
+    socket.on('disconnect', () => {
         console.log('a user disconnected ----------');
     })
 
@@ -46,20 +51,15 @@ io.on('connection',socket=>{
         // Broadcast the received message to all connected clients
         io.emit('broadcastMessage', msg);
     });
-    
+
     // Multiple CLIENT
     // socket.emit('server1', "hello from server1"); 
 })
 
-
-
-
 // listen to http
-http.listen(port, ()=>{
+http.listen(port, localIP, () => {
     console.log(`server has started on port ${port}`)
 })
-
-
 
 // //listen to 'app'
 // app.listen(port , ()=>{
